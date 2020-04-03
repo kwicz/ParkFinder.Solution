359348d9d105f475a8ac26f30d6d1244c35281d7
Katy Solovewicz <katy@solovewicz.com>
2020-04-03 08:47:38 -0700

    add configurations, park model, and context file with seeded data.

---
+++ ParkFinder/Models/Park.cs
@@ -0,0 +1,12 @@
+namespace ParkFinder.Models
+{
+  public class ParkFinder
+  {
+    public int ParkId { get; set; }
+    public string Name { get; set; }
+    public string Location { get; set; }
+    public string DateEstablished { get; set; }
+    public string Area { get; set; }
+    public string Description { get; set; }
+  }
+}
---
+++ ParkFinder/Models/ParkFinderContext.cs
@@ -0,0 +1,23 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace ParkFinder.Models
+{
+  public class ParkFinderContext : DbContext
+  {
+    public ParkFinderContext(DbContextOptions<ParkFinderContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Park> Parks { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+      builder.Entity<Park>()
+          .HasData(
+              new Park { ParkId = 1, Name = "Epicodus", Location = "Portland, OR", DateEstablished = "January 1, 2010", Area = "47,389.67 acres (191.8 km2)", Description = "Set in Downtown Portland, this park feature lots of computers, feral young programmers, and hand sanitizer." }
+    }
+          );
+    }
+  }
+}
